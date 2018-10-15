751aa425e02ad58ee8b517eb255ce6fee6c9bf14
Hannah Kang <kyouyatamax@gmail.com>
2018-10-15 14:35:02 -0700

    renderBook function to display new books as they are added to the library

--- main.js
+++ main.js
@@ -5,40 +5,31 @@ function Book(title,author) {
   this.author=author;
 }
 
-//This is not the right way to make a constructor
-// function Book(title,author) {
-//  //the constructor.. 
-//  const newbook = {
-//    thetitle=title
-//    theauthor=author
-//    readIt=false
-//  }
-// }
-
 function addBookToLibrary() {
   //do stuff here
   var title = prompt("What is the book title?");
   var author = prompt ("What is the author's name?");
   const newBook = new Book(title,author);
   myLibrary.push(newBook);
-  for (i in myLibrary) {
-    console.log(i);
-    console.table(myLibrary);
-  }
+  renderBook(newBook);
+  // for (i in myLibrary) {
+  //   console.log(i);
+  //   console.table(myLibrary);
+  // }
   return myLibrary;
 }
 
-
-
 // This function should make a visible display of each book 
 //as they are added to the library
-function renderBook() {
+
+
+function renderBook(Book) {
   const container=document.querySelector('#container');
   
   const content = document.createElement('div');
   content.classList.add('buttondiv');
   content.textContent='This is a new book';
-  
+  content.textContent=Book.title+Book.author;
   container.appendChild(content);
 
 }
@@ -48,4 +39,5 @@ myLibrary.push(og1);
 const og2= new Book("book2","author2");
 myLibrary.push(og2);
 
-renderBook();
+renderBook(og1);
+addBookToLibrary();
